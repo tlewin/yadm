80ff94009abf319e356f5610785818db3afc7a85
Thiago Lewin <thiago.lewin@gmail.com>
2019-01-06 22:59:17 +0100

    Add some dependencies in dev environment

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [honeysql "0.9.4" :scope "provided"]]
 
-  :profiles {:dev {:dependencies [[nrepl "0.4.5"]]
+  :profiles {:dev {:dependencies [[nrepl "0.4.5"]
+                                  [org.clojure/java.jdbc "0.7.8"]
+                                  [org.postgresql/postgresql "42.2.4"]]
                    :plugins [[cider/cider-nrepl "0.18.0"]]
                    :repl-options {:init-ns yadm.core}}})
